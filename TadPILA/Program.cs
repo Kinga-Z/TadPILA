b78478c2a2674f23ca75041d771f87708bc14af4
ANTHONY GABRIEL ALONSO GOMEZ <e1314620855@live.uleam.edu.ec>
2021-11-25 18:45:11 -0500

    Agregar archivos de proyecto.

---
+++ TadPILA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace conStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClasePila Pila = new ClasePila();
+            Pila.Acceso();
+        }
+    }
+}
